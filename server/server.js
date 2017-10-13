a0003870651747161ae4afba773828a48c027e26
sonamdo <sonamklein@gmail.com>
2017-10-12 21:51:47 -0400

    minor change to server.js file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
- var express = require('express');
-var bodyParser = require('body-parser');//takes JSON and converts to Object, attaching to request objest
+var express = require('express');
+var bodyParser = require('body-parser');//takes JSON and converts to JS object, attaching to request objest
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require ('./models/todo');
@@ -7,19 +7,24 @@ var {User} = require ('./models/user');
 
 var app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json());//Convert json into object, attaching it to req
 
-app.post('/todos',(req, res)=>{
-  var todo = new Todo ({
-    text: req.body.text
-  });
-
-  todo.save().then((doc)=>{
+app.post('/todos',(req, res)=>{//set up a route with app.post to get body data(in the form of req) from client. Sends JSON with req(uest), get it back with res(ponse)
+  var todo = new Todo({//create todo with res(user input)
+    text : req.body.text//sets todo
+  })
+  todo.save().then((doc)=>{//saves new todo, or calls error if problem
     res.send(doc);
-  }, (e) =>{
+  }, (e)=>{
     res.status(400).send(e);
+    });
   });
-});
+
+  // todo.save().then((doc)=>{
+  //   res.send(doc);
+  // }, (e) =>{
+  //   res.status(400).send(e);
+  // });
 
 app.listen(3000, () => {
   console.log('started on port 3000');
