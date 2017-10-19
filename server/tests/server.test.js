3b8694fd48e21e9506dc4e37b4a7e4926831c139
sonamdo <sonamklein@gmail.com>
2017-10-18 22:42:55 -0400

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,19 @@
-//loading our testing tools
 const expect = require('expect');
 const request = require('supertest');
 
-//loading files to test
 const {app} = require('./../server')//load server app variable through relative path
 const {Todo} = require('./../models/todo')
 
+const todos = [{
+  text : 'first test todo'
+},{
+  text : 'second test todo'
+}];
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=> done());//clear Todos before test;
+  Todo.remove({}).then(()=> {
+    return Todo.insertMany(todos);
+  }).then(() => done());//clear Todos before test;
 });//testing lifecycle method, runs code before every test case
 
 //make test cases with describe\
@@ -15,7 +21,7 @@ describe('POST /todos',()=>{
   it('Should create a new todo', (done)=>{//donee argument need for asynchronous test
     var text = 'Test todo text';//setup data
 
-    request (app)
+    request(app)
       .post('/todos')//post request
       .send({text})//sends data from above converted to JSON by supertest
       .expect(200)
@@ -27,14 +33,13 @@ describe('POST /todos',()=>{
           return done(err);
         }
 //2nd test checks what was passed into mongodb is ok
-        Todo.find().then((todos)=>{
+        Todo.find({text}).then((todos)=>{
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
         }).catch((e)=> done(e));//if either expect fails, catch. Otherwisee it will still pass
       });
   });
-});
 //2nd test case, verifies that todo is not created when bad data is sent
 it('should not create todo with invalid body data', (done)=>{
     request(app)
@@ -44,11 +49,24 @@ it('should not create todo with invalid body data', (done)=>{
       .end((err, res) =>{
         if(err){
           return done(err);
-        }
-    Todo.find().then((todos)=>{
-      expect(todos.length).toBe(0);//makes sure no todos were created
-      done()
-    }).catch((e)=>done(e));
+      }
+      Todo.find().then((todos)=>{
+        expect(todos.length).toBe(2);//makes sure no todos were created
+        done();
+      }).catch((e)=>done(e));
+    });
+  });
+});
+//3rd test case, checks that todos exist
+describe('GET /todos', ()=>{
+  it('should get all todos',(done) =>{
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
 
