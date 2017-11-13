496e76783b8e7d7f5fc271018a72ec7470db4831
sonamdo <sonamklein@gmail.com>
2017-11-12 21:29:05 -0500

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -106,7 +106,7 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 //POST /users
-app.post('/users',(req, res) =>{
+app.post('/users',(req, res) =>{//create user call
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
@@ -138,6 +138,21 @@ var authenticate =  (req, res, next) =>{
 app.get('/users/me', authenticate, (req,res)=>{
   res.send(req.user);
 })
+
+//post /users/login (email, password)
+
+app.post('/users/login', (req, res) => {//user login in call
+
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);//attaches token to header
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
